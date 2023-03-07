f7642cbe13d446ffd1cc22f181a13214f3310ec8
Euphoria99 <363pavanbhat@gmail.com>
2023-03-07 23:01:26 +0530

    Local modules

---
+++ node/add.js
@@ -0,0 +1,6 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sum = add(1, 3);
+console.log('🚀 ~ file: add.js:6 ~ sum:', sum);
--- node/app.js
+++ node/app.js
@@ -1,1 +1,3 @@
+require('./add');
+
 console.log('hello from app.js');
