de5f1e35828aeee807c8e5cde2981519cd80816a
Euphoria99 <363pavanbhat@gmail.com>
2023-03-25 23:44:01 +0530

    Pipes

--- node/built-in/newstream.js
+++ node/built-in/newstream.js
@@ -1,13 +1,23 @@
 const fs = require('node:fs');
 
+const zlib = require("node:zlib");
+
+const gzip = zlib.createGzip()
+
+
 const readableStream = fs.createReadStream('./node/built-in/file.txt', {
   encoding: 'utf-8',
   highWaterMark: 2,
 });
 
+readableStream.pipe(gzip).pipe(fs.WriteStream("./node/built-in/file2.txt.gz"))
+
 const writeableStream = fs.createWriteStream('./node/built-in/file2.txt');
 
-readableStream.on('data', (chunk) => {
-  console.log(chunk);
-  writeableStream.write(chunk);
-});
+
+readableStream.pipe(writeableStream);
+
+// readableStream.on('data', (chunk) => {
+//   console.log(chunk);
+//   writeableStream.write(chunk);
+// });
