39806dfe5e11c1919490731760f3a8423168520c
Euphoria99 <363pavanbhat@gmail.com>
2023-03-26 22:44:52 +0530

    Http routing

--- node/nodeTwo/server.js
+++ node/nodeTwo/server.js
@@ -2,11 +2,25 @@ const http = require('node:http');
 const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
-    const name = 'Pavan';
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    let html = fs.readFileSync('./node/nodeTwo/index.html', 'utf-8');
-    html = html.replace('{{name}}', name);
-    res.end(html);
+
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Home Page');
+    } else if (req.url === '/about') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('About Page');
+    } else if (req.url === '/api') {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({
+          firstName: 'foo',
+          lastName: 'bar',
+        })
+      );
+    } else {
+      res.writeHead(404);
+      res.end('Page not found');
+    }
 });
 
 const port = 3000;
