f8d853abdc72ce35df76463ea391dfdda1425145
Euphoria99 <363pavanbhat@gmail.com>
2023-03-28 23:53:01 +0530

    Network I/O , native async mechanism

--- node/nodeTwo/index.js
+++ node/nodeTwo/index.js
@@ -1,6 +1,7 @@
-const crypto = require('node:crypto');
+const https = require('node:https');
+// const crypto = require('node:crypto');
 
-process.env.UV_THREADPOOL_SIZE = 6;
+// process.env.UV_THREADPOOL_SIZE = 6;
 const start = Date.now();
 
 //exp-1
@@ -9,10 +10,18 @@ const start = Date.now();
 // console.log('Hash: ', Date.now() - start);
 
 //exp-2
-const MAX_CALLS = 6;
+const MAX_CALLS = 4;
 for (let i = 0; i < MAX_CALLS; i++) {
-  crypto.pbkdf2('password', 'salt', 10000, 512, 'sha512', () => {
-    console.log(`Hash: ${i + 1}`, Date.now() - start);
-  });
+  https
+    .request('https://www.google.com', (res) => {
+      res.on('data', () => {});
+      res.on('end', () => {
+        console.log(`Request: ${i + 1}`, Date.now() - start);
+      });
+    })
+    .end();
+  // crypto.pbkdf2('password', 'salt', 10000, 512, 'sha512', () => {
+  //   console.log(`Hash: ${i + 1}`, Date.now() - start);
+  // });
 }
 //libuv's thread pool has 4 threads
