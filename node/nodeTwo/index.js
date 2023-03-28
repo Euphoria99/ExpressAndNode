97088c49577ae09c61074db19d483f410015a5ec
Euphoria99 <363pavanbhat@gmail.com>
2023-03-28 23:29:52 +0530

    Experimenting with thread pool size

--- node/nodeTwo/index.js
+++ node/nodeTwo/index.js
@@ -1,9 +1,18 @@
-const fs = require('node:fs');
+const crypto = require('node:crypto');
 
-console.log('First');
+process.env.UV_THREADPOOL_SIZE = 6;
+const start = Date.now();
 
-fs.readFile('./node/nodeTwo/file.txt', 'utf-8', (err, data) => {
-  console.log('file contents');
-});
+//exp-1
+// crypto.pbkdf2Sync('password', 'salt', 10000, 512, 'sha512');
 
-console.log('last');
+// console.log('Hash: ', Date.now() - start);
+
+//exp-2
+const MAX_CALLS = 6;
+for (let i = 0; i < MAX_CALLS; i++) {
+  crypto.pbkdf2('password', 'salt', 10000, 512, 'sha512', () => {
+    console.log(`Hash: ${i + 1}`, Date.now() - start);
+  });
+}
+//libuv's thread pool has 4 threads
